5d917b334639f4e2ae14faa510ca7fecf34697a0
Amanda Boatswain <amanda.boatswainj@gmail.com>
2020-04-02 17:21:08 -0400

    changed location of input files and updated CMS_load_data.py

--- CMS_load_data.py
+++ CMS_load_data.py
@@ -3,12 +3,17 @@ import pickle
 #import pandas as pd
 
 # create a list of all the files we will work with
-file_paths = ["cms_Animals.cpickle", "cms_Herds.cpickle", "cms_Milkings.cpickle"]
+file_paths = ["./input/cms_Animals.cpickle", "./input/cms_Herds.cpickle", "./input/cms_Milkings.cpickle"]
 datasets = []
 
 # load each dataset to a pandas dataframe 
-for i, file in enumerate(file_paths): 
-    datasets.append(pickle.load( open(file_paths[i], "rb" ) ))
+for i, file in enumerate(file_paths):
+    datasets.append(pickle.load(open(file, "rb")))
+    
+    print("Reading File: ", file)
+    headers = list(datasets[i])
+    print("Categories in cms file: ", headers)    
+    
     
     # print an overview of the dataset in the terminal 
     print("Loaded Dataset: " + file)
